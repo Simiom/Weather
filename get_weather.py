98db672042040d24e96cb756ec3f10c2b76f8df9
MarkT5 <mark.turkov@mail.ru>
2018-12-16 16:11:40 +0500

    исправлены ошибки отправки запрома и добавлена функция определения времени по гривичу

--- get_weather.py
+++ get_weather.py
@@ -1,9 +1,9 @@
-
+import datetime
 import time
 import requests
 import json
-
 from geopy.geocoders import Nominatim
+from pytz import country_timezones
 
 all_info = None
 
@@ -11,7 +11,7 @@ def send_request(city):
     geolocator = Nominatim()
     location = geolocator.geocode(city)
     global all_info
-    url = 'https://api.weather.yandex.ru/v1/forecast?info?lat={}&lon={}&extra=false'.format(location.latitude, location.longitude)
+    url = 'https://api.weather.yandex.ru/v1/forecast?lat={}&lon={}&extra=false'.format(location.latitude, location.longitude)
     headers = {'X-Yandex-API-Key': 'd2c28be5-c612-4ddc-b5a2-d2b31e2baa71'}
 
     request = requests.get(url, headers=headers)
@@ -29,17 +29,15 @@ def get_forecast(day, hour):
 
 
 def get_time():
-    if all_info != None:
-        return time.ctime(all_info['fact']['obs_time'])
-    else:
-        print('send request before trying to get info')
+    utc = datetime.datetime.utcnow()
+    return utc.strftime("%d.%m.%Y %H")
 
 
 
 
 
-
-
-send_request("Тюмень")
-print(get_forecast(0,0))
-print(get_time())
+#send_request("London")
+#print(get_forecast(0,0))
+#print(get_time())
+#print(all_info['info'])
+#1544832000
